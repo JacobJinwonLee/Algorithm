bff9818a122fe0b34fab1eaddebebe8af7c9b5f2
Jacob Jinwon Lee <jinwon82037@gmail.com>
2021-05-11 01:32:22 +0900

    Add files via upload

--- LeetCode/Reverse_Linked_List.py
+++ LeetCode/Reverse_Linked_List.py
@@ -6,3 +6,17 @@ Created on Tue May 11 01:01:15 2021
 """
 
 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        node, prev = head, None
+        
+        while node:
+            next, node.next = node.next, prev
+            prev, node = node, next
+        
+        return prev
+        
